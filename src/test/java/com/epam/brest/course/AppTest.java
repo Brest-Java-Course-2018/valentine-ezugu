12ff61e24cecfeaa2eb41fd6a5e4a6f49923d3d1
valentine-ezugu <valentineezugu@yahoo.com>
2018-02-13 01:41:16 +0300

    main method test

--- src/test/java/com/epam/brest/course/AppTest.java
+++ src/test/java/com/epam/brest/course/AppTest.java
@@ -1,38 +1,44 @@
 package com.epam.brest.course;
 
-import junit.framework.Test;
+import junit.framework.Assert;
+
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+import java.io.*;
+
+@RunWith(JUnit4.class)
+public class AppTest extends TestCase {
+
+    /*
+    System.out.println( "Hello World!" );
+    is invoked, behind the scene, it writes
+    in the ByteArrayOutputStream referenced
+    by bos as out is a PrintWriter that decorates
+    this ByteArrayOutputStream
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    //java lang exception is never thrown so we dont need
+    @Test
+    public void mainMethodTest() {
+
+        // to have a way to undo the binding with your `ByteArrayOutputStream`
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(bos));
+
+        // action
+        App.main(null);
+
+        // assertion
+        Assert.assertEquals("Hello World!", bos.toString().trim());
+        // undo the binding in System
+        System.setOut(originalOut);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
