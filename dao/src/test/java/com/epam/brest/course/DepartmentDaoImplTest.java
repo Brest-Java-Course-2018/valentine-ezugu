2b06f6ee57591241bcb2652c8cec6c9415014e09
valentine-ezugu <valentineezugu@yahoo.com>
2018-02-27 02:22:28 +0300

    application context config

---
+++ dao/src/test/java/com/epam/brest/course/DepartmentDaoImplTest.java
@@ -0,0 +1,22 @@
+package com.epam.brest.course;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:test-db-spring.xml"})
+public class DepartmentDaoImplTest {
+
+    @Test
+    public void getDepartments() {
+    }
+
+    @Test
+    public void getDepartmentById() {
+    }
+}
